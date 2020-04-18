7bac5d270a84fe29c1b0b201ff98325ff28393a4
Peter Li <peter@gigasciencejournal.com>
2020-04-18 20:15:42 +0800

    Escape "grant" in databse migration script
    This migration script does not work since "grant" is command in
    PostgreSQL which the "grant" column name must be conflicting with.
    Escaping "grant" fixes this problem.

--- protected/migrations/m200418_072727_subwiz_create_funding_table.php
+++ protected/migrations/m200418_072727_subwiz_create_funding_table.php
@@ -9,7 +9,7 @@ class m200418_072727_subwiz_create_funding_table extends CDbMigration
             dataset_id integer NOT NULL,
             funder_id integer NOT NULL,
             program_name character varying(100) NOT NULL,
-            grant character varying(100) NOT NULL,
+            \"grant\" character varying(100) NOT NULL,
             pi_name character varying(100) NOT NULL);
         ");
 
