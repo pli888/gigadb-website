c06775a89a8782731e92a772ffa86c7a0a5d4545
Peter Li <peter@gigasciencejournal.com>
2020-05-20 18:42:08 +0800

    Update status names and display thank you message after submission
    Function actionSubmit() updated so that conditions for
    upload_statusconditional checks have changed: Incomplete >
    UserStartedIncomplete and Request > Submitted. Modification date
    now added for dataset. The redirect for this function has changed
    from "/user/view_profile#submitted" to
    "/user/view_profile/thanks/1/added/" page.
    
    In view_profile user view class has a ternary operator to check
    for "thanks" variable and if it has value = "1". If so then a
    "thank you for your dataset" message is displayed.
    
    This commit is a port of 5ffe6f3e After Complete submission from
    lump1k-sergey branch.

--- protected/controllers/DatasetSubmissionController.php
+++ protected/controllers/DatasetSubmissionController.php
@@ -94,7 +94,7 @@ class DatasetSubmissionController extends Controller
             $this->isSubmitter($dataset);
 
             $isOld = 1;
-            if ($dataset->upload_status == 'Incomplete') {
+            if ($dataset->upload_status == 'UserStartedIncomplete') {
                 $isOld = 0;
             }
 
@@ -102,10 +102,11 @@ class DatasetSubmissionController extends Controller
                 $dataset->upload_status = 'Pending';
                 CurationLog::createlog($dataset->upload_status, $dataset->id);
             } else {
-                $dataset->upload_status = 'Request';
+                $dataset->upload_status = 'Submitted';
                 CurationLog::createlog($dataset->upload_status, $dataset->id);
             }
 
+            $dataset->modification_date = date('Y-m-d');
             if (!$dataset->save(false)) {
                 Yii::app()->user->setFlash('keyword', "Submit failure" . $dataset_id);
                 $this->redirect("/user/view_profile");
@@ -121,7 +122,7 @@ class DatasetSubmissionController extends Controller
             MailHelper::sendUpdateDatasetToAdmin($user, $dataset);
         }
 
-        $this->redirect('/user/view_profile#submitted');
+        $this->redirect('/user/view_profile/thanks/1/added/' . $dataset->id . '#submitted');
     }
 
     public function actionUpdateSubmit()
