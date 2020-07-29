ec03b86062cffa908904acf7c33c5bfc31e28618
Peter Li <peter@gigasciencejournal.com>
2020-07-29 17:23:36 +0800

    Add migration file to alter external_link table

--- protected/migrations/schema/m200529_040200_subwiz_alter_external_link_tab.php
+++ protected/migrations/schema/m200529_040200_subwiz_alter_external_link_tab.php
@@ -1,18 +1,19 @@
 <?php
 
-class m200417_050712_subwiz_alter_external_link_table_add_cols extends CDbMigration
+class m200529_040200_subwiz_alter_external_link_tab extends CDbMigration
 {
     public function up()
     {
+        // @only1chunts says: We want to be able to add user defined
+        // descriptions to the links to display on the dataset pages. #61 github
+        // issue
         $this->execute("ALTER TABLE external_link
-            ALTER external_link_type_id DROP NOT NULL,
             ADD description character varying(200) NULL;");
     }
 
     public function down()
     {
         $this->execute("ALTER TABLE external_link
-            ALTER external_link_type_id SET NOT NULL,
             DROP description;");
     }
 }
