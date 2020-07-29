861913bf712f611572969eb9e12a4b92c10f63f0
Peter Li <peter@gigasciencejournal.com>
2020-07-29 17:30:08 +0800

    Add migration to alter prefix table

--- protected/migrations/schema/m200529_040250_subwiz_alter_prefix_tab.php
+++ protected/migrations/schema/m200529_040250_subwiz_alter_prefix_tab.php
@@ -1,9 +1,12 @@
 <?php
 
-class m200417_053925_subwiz_alter_prefix_table_add_regexp_col extends CDbMigration
+class m200529_040250_subwiz_alter_prefix_tab extends CDbMigration
 {
     public function up()
     {
+        // @only1chunts says: I did ask for RegEx testing of the input for
+        // certain fields so it makes sense to have those regular expressions
+        // stored somewhere
         $this->execute("ALTER TABLE prefix
             ADD regexp character varying(128) NULL;");
     }
