00169f9681387f914fea128d58eb82c0a64bc03f
Peter Li <peter@gigasciencejournal.com>
2020-08-13 07:38:46 +0800

    Disable links in subwiz tabs when creating new dataset
    The _tabs_navigation view class checks if the dataset model is a
    new record. If so, then Author, Additional Information, Funding and
    Sample tabs do not have links to their respective pages.
    
    This commit relates to gigascience/gigadb-website#389

--- protected/views/datasetSubmission/_tabs_navigation.php
+++ protected/views/datasetSubmission/_tabs_navigation.php
@@ -14,11 +14,11 @@ $isTest = isset($_GET['is_test']) && $_GET['is_test'] == '1' ? '/is_test/1' : ''
    class="btn <?= $controller == 'datasetSubmission' && ($action == 'create1' || $action == 'datasetManagement') ? 'sw-selected-btn' : 'nomargin' ?>"><?= Yii::t('app' , 'Study')?></a>
 <a href="<?= $model->getIsNewRecord() ? '#' : "/datasetSubmission/authorManagement{$modelId}{$isTest}" ?>"
    class="btn <?= $action == 'authorManagement' ? 'sw-selected-btn' : 'nomargin' ?>"><?= Yii::t('app' , 'Author')?></a>
-<a href="/datasetSubmission/additionalManagement<?= $modelId . $isTest ?>"
+<a href="<?= $model->getIsNewRecord() ? '#' : "/datasetSubmission/additionalManagement{$modelId}{$isTest}" ?>"
    class="btn <?= $action == 'additionalManagement' ? 'sw-selected-btn' : 'nomargin' ?>"><?= Yii::t('app' , 'Additional Information')?></a>
-<a href="/datasetSubmission/fundingManagement<?= $modelId . $isTest ?>"
+<a href="<?= $model->getIsNewRecord() ? '#' : "/datasetSubmission/fundingManagement{$modelId}{$isTest}" ?>"
    class="btn <?= $action == 'fundingManagement' ? 'sw-selected-btn' : 'nomargin' ?>"><?= Yii::t('app' , 'Funding')?></a>
-<a href="/datasetSubmission/sampleManagement<?= $modelId . $isTest ?>"
+<a href="<?= $model->getIsNewRecord() ? '#' : "/datasetSubmission/sampleManagement{$modelId}{$isTest}" ?>"
    class="btn <?= $action == 'sampleManagement' || $action == 'end' ? 'sw-selected-btn' : 'nomargin' ?>"><?= Yii::t('app' , 'Sample')?></a>
 <?php if($controller == 'adminFile' && $action == 'create1' || $model->upload_status == 'UserUploadingData' ): ?>
     <a href="/adminFile/create1<?= $modelId . $isTest ?>" class="btn <?= $controller == 'adminFile' && $action == 'create1' ? 'sw-selected-btn' : 'nomargin' ?>"><?= Yii::t('app' , 'File')?></a>
