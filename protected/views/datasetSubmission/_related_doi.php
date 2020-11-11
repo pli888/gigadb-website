09f813201fcd60a883c22020c116c39cead71da0
Peter Li <peter@gigasciencejournal.com>
2020-11-11 15:41:21 +0800

    Update layout for related gigadb datasets

--- protected/views/datasetSubmission/_related_doi.php
+++ protected/views/datasetSubmission/_related_doi.php
@@ -1,12 +1,9 @@
 <div class="form-horizontal additional-bordered">
     <h3 style="display: inline-block">Related GigaDB Datasets</h3>
     <a class="myHint" style="float: none;" data-content="If this dataset is directly related to any other previously published GigaDB dataset, for example an update or extension to, please provide the dataset ID and the relationship to complete the sentence below."></a>
-
-
     <p class="note">
         Does this dataset use or relate to any other GigaDB dataset?
     </p>
-
     <div style="text-align: center; margin-bottom: 15px;">
         <a href="#"
            data-target="related-doi"
@@ -20,52 +17,58 @@
            data-id="<?= $model->id ?>"
            class="btn additional-button <?php if ($isRelatedDoi === false): ?>btn-green btn-disabled<?php else: ?>js-no-button<?php endif; ?>"/>No</a>
     </div>
-
     <div id="related-doi"<?php if ($isRelatedDoi !== true): ?> style="display: none"<?php endif; ?>>
-        <div class="control-group" style="text-align: right;">
-            <label>The dataset I am now uploading</label>
-            <?= CHtml::dropDownList('relation', null,
-                array('' => 'Please select') + CHtml::listData(Relationship::model()->findAll(), 'id', 'name'),
-                array('class'=>'js-relation-relationship dropdown-white','style'=>'width:250px')); ?>
-            <label>dataset (DOI)</label>
-            <?= CHtml::dropDownList('dataset_doi', null,
-                array('' => 'Please select') + CHtml::listData(Util::getDois(), 'identifier', 'identifier'),
-                array('class'=>'js-relation-doi dropdown-white','style'=>'width:250px')); ?>
-            <a href="#" dataset-id="<?=$model->id?>" class="btn js-not-allowed"/>Add Related Doi</a>
-        </div>
-
-        <div class="grid-view">
-            <table class="table table-bordered">
-                <thead>
-                <tr>
-                    <th id="author-grid_c0" width="45%">Relationship</th>
-                    <th id="author-grid_c0" width="45%">Related DOI</th>
-                    <th id="author-grid_c5" class="button-column" width="10%"></th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php foreach($relations as $relation): ?>
-                    <tr class="odd js-my-item" id="js-relation-<?=$relation->id?>">
-                        <td><?= $relation->relationship->name ?></td>
-                        <td><?= $relation->related_doi ?></td>
-                        <td class="button-column">
-                            <input type="hidden" class="js-relationship-id" value="<?= $relation->relationship->id ?>">
-                            <input type="hidden" class="js-my-id" value="<?= $relation->id ?>">
-                            <a class="js-delete-relation delete-title" relation-id="<?=$relation->id?>" data-id="<?= $model->id ?>" title="delete this row">
-                                <img alt="delete this row" src="/images/delete.png">
-                            </a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-
-                <tr class="js-no-results"<?php if ($relations): ?> style="display: none"<?php endif ?>>
-                    <td colspan="4">
-                        <span class="empty">No results found.</span>
-                    </td>
-                </tr>
-                <tr>
-                </tbody>
-            </table>
+        <div class="row subwiz-row">
+            <div class="col-xs-12">
+                <div class="control-group" style="text-align: center;">
+                    <label>The dataset I am now uploading</label>
+                    <?= CHtml::dropDownList('relation', null,
+                        array('' => 'Please select') + CHtml::listData(Relationship::model()->findAll(), 'id', 'name'),
+                        array('class'=>'js-relation-relationship form-control','style'=>'width:250px')); ?>
+                    <label>dataset (DOI)</label>
+                    <?= CHtml::dropDownList('dataset_doi', null,
+                        array('' => 'Please select') + CHtml::listData(Util::getDois(), 'identifier', 'identifier'),
+                        array('class'=>'js-relation-doi form-control','style'=>'width:250px')); ?>
+                    <a href="#" dataset-id="<?=$model->id?>" class="btn background-btn js-not-allowed"/>Add Related Doi</a>
+                </div>
+                <div class="row subwiz-row">
+                    <div class="col-xs-12">
+                        <div class="grid-view">
+                            <table class="table table-bordered">
+                                <thead>
+                                <tr>
+                                    <th id="author-grid_c0" width="45%">Relationship</th>
+                                    <th id="author-grid_c0" width="45%">Related DOI</th>
+                                    <th id="author-grid_c5" class="button-column" width="10%"></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php foreach($relations as $relation): ?>
+                                    <tr class="odd js-my-item" id="js-relation-<?=$relation->id?>">
+                                        <td><?= $relation->relationship->name ?></td>
+                                        <td><?= $relation->related_doi ?></td>
+                                        <td class="button-column">
+                                            <input type="hidden" class="js-relationship-id" value="<?= $relation->relationship->id ?>">
+                                            <input type="hidden" class="js-my-id" value="<?= $relation->id ?>">
+                                            <a class="js-delete-relation delete-title" relation-id="<?=$relation->id?>" data-id="<?= $model->id ?>" title="delete this row">
+                                                <img alt="delete this row" src="/images/delete.png">
+                                            </a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                
+                                <tr class="js-no-results"<?php if ($relations): ?> style="display: none"<?php endif ?>>
+                                    <td colspan="4">
+                                        <span class="empty">No results found.</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
