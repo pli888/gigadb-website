55004c4f8990894001752722da88d8201401f963
Peter Li <peter@gigasciencejournal.com>
2020-11-11 19:53:25 +0800

    Fix save functionality in Add Info tab

--- protected/views/datasetSubmission/additionalManagement.php
+++ protected/views/datasetSubmission/additionalManagement.php
@@ -101,9 +101,9 @@ $disabled = $isSources === null || $isCodes === null || $isRepositories === null
             $('#' + nextBlock).show();
         }
 
-        $this.addClass('btn background-btn');
+        $this.addClass('btn-green btn-disabled');
         $this.removeClass('js-no-button');
-        $this.siblings().removeClass('btn background-btn').addClass('js-yes-button');
+        $this.siblings().removeClass('btn-green btn-disabled').addClass('js-yes-button');
 
         if (type) {
             if (
@@ -139,9 +139,9 @@ $disabled = $isSources === null || $isCodes === null || $isRepositories === null
         var $this = $(this);
         var target = $this.data('target');
 
-        $this.addClass('btn background-btn');
+        $this.addClass('btn-green btn-disabled');
         $this.removeClass('js-yes-button');
-        $this.siblings('a').removeClass('btn background-btn').addClass('js-no-button');
+        $this.siblings('a').removeClass('btn-green btn-disabled').addClass('js-no-button');
 
         $('#' + target).show();
 
