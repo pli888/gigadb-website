49805dc7022c58b08e08244263d511d9fc134a49
Peter Li <peter@gigasciencejournal.com>
2020-11-17 17:11:35 +0800

    WIP Add documentation in ImageHaver class

--- protected/models/ImageHaver.php
+++ protected/models/ImageHaver.php
@@ -6,6 +6,15 @@ class ImageHaver extends CActiveRecord {
     public $thumbSize = 120;
     public $smallThumbSize = 64;
 
+    /**
+     * Create directories for images to be uploaded and created in
+     *
+     * $dir will be ../../images/uploads/image_upload
+     * $dir_thumbs will be ../../images/uploads/thumbs
+     * $dir_small_thumbs will be ../../images/uploads/small_thumbs
+     *
+     * @param $type
+     */
     public function createDirs($type) {
         $dir = Yii::getPathOfAlias('uploadPath') . "/{$type}";
         $dir_thumbs = "$dir/thumbs";
@@ -15,7 +24,13 @@ class ImageHaver extends CActiveRecord {
         if (!file_exists($dir_small_thumbs)) mkdir($dir_small_thumbs);
     }
 
-    # Path to file without leading directory or URL
+    /**
+     * Return path to file without leading directory or URL
+     *
+     * @param $type
+     * @param string $size image size
+     * @return string $path
+     */
     public function getPath($type, $size='') {
         $class = get_class($this);
         $dir = "/$type";
@@ -34,32 +49,71 @@ class ImageHaver extends CActiveRecord {
         return $path;
     }
 
+    /**
+     * Returns absolute path?
+     *
+     * @param $type
+     * @param string $size
+     * @return string
+     */
     public function getFullPath($type, $size="") {
         return Yii::getPathOfAlias('uploadPath') . $this->getPath($type, $size);
     }
 
+    /**
+     * Returns URL for image which will be /images/uploads/
+     *
+     * @param $type
+     * @param string $size
+     * @return string
+     */
     public function getUrl($type, $size="") {
         return Yii::app()->request->baseURL . Yii::getPathOfAlias('uploadURL') . $this->getPath($type, $size);
     }
 
+    /**
+     * Returns URL for full size image file
+     *
+     * @param $type
+     * @return string|null
+     */
     public function image($type) {
         $path = $this->getFullPath($type);
         if (!file_exists($path)) return null;
         return $this->getUrl($type);
     }
 
+    /**
+     * Returns URL for thumbnail images
+     *
+     * @param $type
+     * @return string|null
+     */
     public function thumb($type) {
         $path = $this->getFullPath($type, 'thumb');
         if (!file_exists($path)) return null;
         return $this->getUrl($type, 'thumb');
     }
 
+    /**
+     * Returns URL for small thumbnail images
+     *
+     * @param $type
+     * @return string|null
+     */
     public function smallThumb($type) {
         $path = $this->getFullPath($type, 'small_thumb');
         if (!file_exists($path)) return null;
         return $this->getUrl($type, 'small_thumb');
     }
 
+    /**
+     * Saves full size image and creates thumbnails of it
+     *
+     * @param $type
+     * @param $image
+     * @return bool
+     */
     public function setImage($type, $image) {
         $path = $this->getFullPath($type);
         $thumbPath = $this->getFullPath($type, 'thumb');
@@ -84,7 +138,11 @@ class ImageHaver extends CActiveRecord {
         }
     }
 
-    # Generate new thumbnails from existing image
+    /**
+     * Generate new thumbnails from existing image
+     *
+     * @param $type
+     */
     public function resizeImages($type) {
         $path = $this->getFullPath($type);
         $thumbPath = $this->getFullPath($type, 'thumb');
@@ -101,6 +159,12 @@ class ImageHaver extends CActiveRecord {
 
 
   // Where should this go?
+
+  /**
+   * Creates file selector for choosing dataset image file
+   *
+   * @param $type
+   */
   public function imageChooserField($type) {
     $image = $this->image($type);
 
@@ -130,6 +194,12 @@ class ImageHaver extends CActiveRecord {
   }
 
     // Or this, for that matter
+
+    /**
+     * Update image for dataset given an image is already present in dataset
+     *
+     * @param $type
+     */
     public function updateImage($type) {
         if (!isset($_POST["use_$type"]) or $_POST["use_$type"]!= 'current') {
     	    $image = CUploadedFile::getInstanceByName("{$type}_image");
@@ -140,30 +210,65 @@ class ImageHaver extends CActiveRecord {
         }
     }
 
+    /**
+     * Returns HTML image tag
+     *
+     * @param $type
+     * @param string $alt
+     * @return string
+     */
     public function imageTag($type, $alt='') {
         $url = $this->image($type);
         if (!$url) return $this->defaultImage($type);
         return "<img src='$url' alt='$alt' />";
     }
 
+    /**
+     * Returns HTML image tag with thumbTag class value
+     *
+     * @param $type
+     * @param string $alt
+     * @return string
+     */
     public function thumbTag($type, $alt='') {
       $url = $this->thumb($type);
       if (!$url) return $this->thumbDefaultImage($type);
       return "<img src='$url' alt='$alt' class='thumbTag' />";
     }
 
+    /**
+     * Returns HTML image tag with thumbTag class value
+     *
+     * @param $type
+     * @param string $alt
+     * @return string
+     */
     public function smallThumbTag($type, $alt='') {
         $url = $this->smallThumb($type);
         if (!$url) return $this->smallDefaultImage($type);
         return "<img src='$url' alt='$alt' class='thumbTag' />";
     }
 
+    /**
+     * Returns HTML image tag with given size
+     *
+     * @param $type
+     * @param string $alt
+     * @return string
+     */
     public function featureTitleTag($type, $alt='') {
         $url = $this->smallThumb($type);
         if ($url)
           return "<img src='$url' alt='$alt' height='18' width='18' />";
     }
 
+    /**
+     * Returns HTML image tag
+     *
+     * @param $type
+     * @param string $alt
+     * @return string
+     */
     public function defaultImage($type, $alt='') {
         if (!$alt) {
             $alt = "anonymous $type";
@@ -172,6 +277,13 @@ class ImageHaver extends CActiveRecord {
         return "<img src='$url' alt='$alt' />";
     }
 
+    /**
+     * Returns HTML image tag for anonymous images
+     *
+     * @param $type
+     * @param string $alt
+     * @return string
+     */
     public function thumbDefaultImage($type, $alt='') {
         if (!$alt) {
             $alt = "anonymous $type";
@@ -180,6 +292,13 @@ class ImageHaver extends CActiveRecord {
         return "<img src='$url' alt='$alt' class='thumbTag' />";
     }
 
+    /**
+     * Returns HTML image tag for small anonymous images
+     *
+     * @param $type
+     * @param string $alt
+     * @return string
+     */
     public function smallDefaultImage($type, $alt='') {
         if (!$alt) {
             $alt = "anonymous $type";
@@ -188,6 +307,14 @@ class ImageHaver extends CActiveRecord {
         return "<img src='$url' alt='$alt' />";
     }
 
+    /**
+     * Creates a new image with custom size in a given directory
+     *
+     * @param $fromPath
+     * @param $toPath
+     * @param $width
+     * @param $height
+     */
     public function transformImage($fromPath, $toPath, $width, $height) {
         $image = Yii::app()->image->load($fromPath);
         if ($image) {
