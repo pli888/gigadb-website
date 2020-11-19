e1926a0ea5205a1d223fa4c241a65cff2dfa4c34
Peter Li <peter@gigasciencejournal.com>
2020-11-19 15:54:41 +0800

    WIP fix WL behat tests for Author tab in submission wizard

---
+++ sql/subwiz_study.sql
@@ -0,0 +1,6 @@
+INSERT INTO dataset (id, submitter_id, image_id, identifier, title, description, dataset_size, ftp_site,
+                        upload_status, excelfile, excelfile_md5, publication_date, modification_date, publisher_id,
+                        token, fairnuse, curator_id, manuscript_id, handing_editor, additional_information, funding,
+                        is_test, creation_date, is_deleted)
+VALUES (300, 344, 225, '100006', 'adfa', '', null, '', 'UserStartedIncomplete', null, null, null, null, null, null,
+        null, null, '', null, null, null, 0, '2020-11-19', null);
