344d889f16527bf4752b0481c57354a7c6f25c34
Peter Li <peter@gigasciencejournal.com>
2020-11-27 08:14:50 +0800

    Fix Public data archive links section
    Applied .col-xs grid to section. Also fixed behaviour of Add link
    button.

--- protected/views/datasetSubmission/_public_links.php
+++ protected/views/datasetSubmission/_public_links.php
@@ -5,11 +5,10 @@
 <div class="form-horizontal additional-bordered">
     <h3 style="display: inline-block">Public data archive links</h3>
     <a class="myHint" style="float: none;" data-content="You should include top level accessions only, e.g. If you add the BioProject accession there is no need to add every BioSample accession contained within that BioProject."></a>
-
-
-    <p class="note">
-        Have you already submitted data to a public repository that is directly described as part of this dataset? E.g. raw sequence data submitted to the Sequence Read Archives.
-    </p>
+    
+    <p class="note">Have you already submitted data to a public repository that 
+    is directly described as part of this dataset? E.g. raw sequence data 
+    submitted to the Sequence Read Archives.</p>
 
     <div style="text-align: center; margin-bottom: 15px;">
         <!-- If $isPublicLinks is null then YES button not disabled -->
@@ -20,7 +19,7 @@
         <a href="#"
            data-target="public-links"
            id="public-links-yes"
-           class="btn btn-default additional-button <?php if ($isPublicLinks === true): ?>btn-success btn-disabled<?php else: ?>js-yes-button<?php endif; ?>"/>Yes</a>
+           class="btn btn-default additional-button <?php if ($isPublicLinks === true): ?>btn-success disabled <?php else: ?>js-yes-button<?php endif; ?>"/>Yes</a>
         <!-- If $isPublicLinks is null then NO button not disabled, js-no-button -->
         <a href="#"
            data-target="public-links"
@@ -28,75 +27,81 @@
            data-url="/adminLink/deleteLinks"
            id="public-links-no"
            data-id="<?= $model->id ?>"
-           class="btn btn-default additional-button <?php if ($isPublicLinks === false): ?>btn-success btn-disabled<?php else: ?>js-no-button<?php endif; ?>"/>No</a>
+           class="btn btn-default additional-button <?php if ($isPublicLinks === false): ?>btn-success disabled <?php else: ?>js-no-button<?php endif; ?>"/>No</a>
     </div>
 
     <!-- For displaying form to add new public link -->
     <div id="public-links"<?php if ($isPublicLinks !== true): ?> style="display: none"<?php endif; ?>>
-        <p class="note">Please select the appropriate database from the list (you may repeat process to add different database links).</p>
+        <div class="row subwiz-row">
+            <div class="col-xs-12">
+                <p class="note">Please select the appropriate database from the 
+                list (you may repeat process to add different database links).</p>
 
-        <div class="control-group">
-            <label class='control-label'>Database</label>
-            <a class="myHint" data-html="true" data-content="Please contact <a href=&quot;mailto:database@gigasciencejournal.com&quot; >database@gigasciencejournal.com</a> to request the addition of a new database."></a>
-            <!-- Render dropdown list of database name acronyms -->
-            <div class="controls">
-                <?= CHtml::dropDownList('prefix',
-                    null,
-                    array('' => 'Please select') + CHtml::listData($link_database,'prefix','prefix'),
-                    array('class'=>'js-database dropdown-white', 'style'=>'width:250px'));
-                ?>
+                <div class="control-group">
+                    <label class='control-label' for="prefix">Database</label>
+                    <a class="myHint" data-html="true" data-content="Please contact <a href=&quot;mailto:database@gigasciencejournal.com&quot; >database@gigasciencejournal.com</a> to request the addition of a new database."></a>
+                    <!-- Render dropdown list of database name acronyms -->
+                    <div class="controls">
+                        <?= CHtml::dropDownList('prefix', null, array('' => 'Please select') + CHtml::listData($link_database,'prefix','prefix'), array('class'=>'js-database form-control', 'style'=>'width:250px')); ?>
+                    </div>
+                </div>
             </div>
         </div>
-
-        <p class="note js-set-database">Please select add accession numbers to your data in the above database</p>
-
-        <div class="control-group js-set-database">
-            <label class='control-label'>Accession number</label>
-            <a class="myHint" data-content="Please provide unique identifier of linked data, e.g. an SRA accession; SRS012345."></a>
-            <div class="controls">
-                <?= CHtml::textField('link', '', array('class'=>'js-acc-num', 'size' => 60, 'maxlength' => 100, 'style'=>'width:240px', 'placeholder'=>"Unique identifier of linked data")); ?>
-                <!-- Add Link button rendered as request not executed with class js-not-allowed -->
-                <a href="#" dataset-id="<?=$model->id?>" class="btn js-not-allowed" style="margin-left: 20px;"/>Add Link</a>
+        <div class="row subwiz-row">
+            <div class="col-xs-12">
+                <p class="note js-set-database">Please select add accession 
+                numbers to your data in the above database</p>
+        
+                <div class="control-group js-set-database">
+                    <label class='control-label' for="link">Accession number</label>
+                    <a class="myHint" data-content="Please provide unique identifier of linked data, e.g. an SRA accession; SRS012345."></a>
+                    <div class="controls">
+                        <?= CHtml::textField('link', '', array('class'=>'js-acc-num form-control', 'size' => 60, 'maxlength' => 100, 'style'=>'width:240px', 'placeholder'=>"Unique identifier of linked data")); ?>
+                        <a href="#" id="add-link" dataset-id="<?=$model->id?>" class="btn btn-success disabled js-not-allowed" style="margin-left: 20px;"/>Add Link</a>
+                    </div>
+                </div>
             </div>
         </div>
-
-        <!-- For displaying created Links to dataset in other databases in table -->
-        <div class="grid-view">
-            <table class="table table-bordered">
-                <thead>
-                <tr>
-                    <th id="author-grid_c0" width="45%">Link Type</th>
-                    <th id="author-grid_c0" width="45%">Link</th>
-                    <th id="author-grid_c5" class="button-column" width="10%"></th>
-                </tr>
-                </thead>
-                <tbody>
-                <!-- Loop thru $links -->
-                <?php foreach($links as $link): ?>
-                    <!-- Parse each link and display information in row -->
-                    <?php $pos = strpos($link->link, ':') ?>
-                    <!-- Add js-my-item class to each row. This class name is used -->
-                    <!-- by js-no-button for deleting rows if NO button is pressed -->
-                    <tr class="odd js-my-item">
-                        <td><?= substr($link->link, 0, $pos) ?></td>
-                        <td><?= substr($link->link, $pos + 1) ?></td>
-                        <!-- Renders delete button for link row -->
-                        <td class="button-column">
-                            <input type="hidden" class="js-my-id" value="<?= $link->id ?>">
-                            <a class="js-delete-link delete-title" link-id="<?=$link->id?>" data-id="<?= $model->id ?>" title="delete this row">
-                                <img alt="delete this row" src="/images/delete.png">
-                            </a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-                <tr class="js-no-results"<?php if ($links): ?> style="display: none"<?php endif ?>>
-                    <td colspan="4">
-                        <span class="empty">No results found.</span>
-                    </td>
-                </tr>
-                <tr>
-                </tbody>
-            </table>
+        <div class="row subwiz-row">
+            <div class="col-xs-12">
+                <div class="grid-view">
+                    <table class="table table-bordered">
+                        <thead>
+                        <tr>
+                            <th id="author-grid_c0" width="45%">Link Type</th>
+                            <th id="author-grid_c0" width="45%">Link</th>
+                            <th id="author-grid_c5" class="button-column" width="10%"></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Loop thru $links -->
+                            <?php foreach($links as $link): ?>
+                                <!-- Parse each link and display information in row -->
+                                <?php $pos = strpos($link->link, ':') ?>
+                                <!-- Add js-my-item class to each row. This class name is used -->
+                                <!-- by js-no-button for deleting rows if NO button is pressed -->
+                                <tr class="odd js-my-item">
+                                    <td><?= substr($link->link, 0, $pos) ?></td>
+                                    <td><?= substr($link->link, $pos + 1) ?></td>
+                                    <!-- Renders delete button for link row -->
+                                    <td class="button-column">
+                                        <input type="hidden" class="js-my-id" value="<?= $link->id ?>">
+                                        <a href="#" id="delete-link" class="js-delete-link delete-title" link-id="<?=$link->id?>" data-id="<?= $model->id ?>" title="delete this row">
+                                            <img alt="delete this row" src="/images/delete.png">
+                                        </a>
+                                    </td>
+                                </tr>
+                                <?php endforeach; ?>
+                                <tr class="js-no-results"<?php if ($links): ?> style="display: none"<?php endif ?>>
+                                    <td colspan="4">
+                                        <span class="empty">No results found.</span>
+                                    </td>
+                                </tr>
+                            <tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -132,9 +137,9 @@
             $('#prefix', publicLinksDiv).val()
             && $('input[name="link"]', publicLinksDiv).val()
         ) {
-            $('.js-not-allowed', publicLinksDiv).removeClass('js-not-allowed').addClass('js-add-link btn-success');
+            $('.js-not-allowed', publicLinksDiv).removeClass('disabled js-not-allowed').addClass('js-add-link');
         } else {
-            $('.js-add-link', publicLinksDiv).removeClass('js-add-link btn-success').addClass('js-not-allowed');
+            $('.js-add-link', publicLinksDiv).removeClass('js-add-link').addClass('disabled js-not-allowed');
         }
     }
 
@@ -174,7 +179,7 @@
                         '<td>' + response.link['link_type'] + '</td>' +
                         '<td>' + response.link['link'] + '</td>' +
                         '<td class="button-column">' +
-                        '<a class="js-delete-link delete-title" title="delete this row">' +
+                        '<a href="#" id="delete-link" class="js-delete-link delete-title" title="delete this row">' +
                         '<img alt="delete this row" src="/images/delete.png">' +
                         '</a>' +
                         '</td>' +
@@ -184,7 +189,7 @@
                     $('.js-no-results', publicLinksDiv).hide();
 
                     $('input[name="link"]', publicLinksDiv).val('');
-                    $('.js-add-link', publicLinksDiv).removeClass('js-add-link btn-success').addClass('js-not-allowed');
+                    $('.js-add-link', publicLinksDiv).removeClass('js-add-link').addClass('disabled js-not-allowed');
 
                     $('#related-doi-block').show();
 
